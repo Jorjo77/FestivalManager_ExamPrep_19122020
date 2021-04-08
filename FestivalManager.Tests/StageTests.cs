f0470a9a4811815a27eac998394ec9762e18463b
Georgi Kolev <tuvkontrol55@gmail.com>
2021-04-08 16:53:16 +0300

    TaskUpdate

--- FestivalManager.Tests/StageTests.cs
+++ FestivalManager.Tests/StageTests.cs
@@ -3,6 +3,7 @@
 // Test ONLY the Stage class. 
 namespace FestivalManager.Tests
 {
+    using FestivalManager.Entities;
     using NUnit.Framework;
     using System;
 
@@ -10,9 +11,52 @@ namespace FestivalManager.Tests
 	public class StageTests
     {
 		[Test]
-	    public void test1()
+	    public void ConstructorShouldInitializeCollections()
 	    {
+			//Arrange
+			Song song = new Song("A", new TimeSpan(0, 3, 30));
+			Performer performer = new Performer("An", "B", 20);
+			Stage stage = new Stage();
+
+
+			//Act 
+			stage.AddPerformer(performer);
+			stage.AddSong(song);
+			stage.AddSongToPerformer("A", "An B");
+			var expectedResult = 1;
+			var actualResult = performer.SongList.Count;
+
+			//Assert
+			Assert.IsNotNull(stage.Performers);
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+		[Test]
+		public void PropertyShouldWorksCorrectly()
+		{
+			//Arrange
+			Performer performer = new Performer("An", "B", 20);
+			Stage stage = new Stage();
+
+			//Act 
+			stage.AddPerformer(performer);
+
+
+			var expectedResult = 1;
+			var actualResult = stage.Performers.Count;
+
+			//Assert
+			Assert.AreEqual(expectedResult, actualResult);
 		}
 
+		[Test]
+		public void MethodAddPerformerShouldThrowArgumentNullExceptionIfReceiveNullArgument()
+        {
+			//Arrange
+			Performer performer = new Performer(null, "B", 20);
+			Stage stage = new Stage();
+
+			//Act - Assert
+			Assert.Throws<ArgumentNullException>(() => stage.AddPerformer(performer));
+		}
 	}
 }
